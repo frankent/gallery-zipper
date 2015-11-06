78542d24d32b5f3baae78bc9cff0ae625b311c2a
Keittirat S <keittirat@outlook.com>
2015-11-07 03:51:40 +0700

    clear config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead','*.dev'),
-        'host_prodution' => array('server43.hostinger.in.th')
+        'host_prodution' => array('*.com')
 
 ));
 
